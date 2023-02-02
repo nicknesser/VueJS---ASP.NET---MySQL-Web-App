69b48a979afe9409e572322ff1b68b21787120dd
nicknesser <nicknesser@gmail.com>
2023-02-01 23:06:03 -0500

    Updated app.js and index.html to work

--- VueMySqlUI/app.js
+++ VueMySqlUI/app.js
@@ -4,6 +4,33 @@ const routes=[
     {path:'/department',component:department}
 ]
 
+/*initialize the router, use the router, mount the app*/
+
+const router = VueRouter.createRouter({
+    history: VueRouter.createWebHashHistory(),
+    routes
+  })
+  const app = Vue.createApp({})
+  app.use(router)
+  app.mount('#app')
+
+  /* Old code, dated based on VueJS 2*
+  const router = new VueRouter({
+    routes
+})
+
 const app = new Vue({
     router
-}).$mount('#app')
+}).$mount('#app')*/
+
+
+/*pulled from VueJS 3* docs
+const app = createApp({
+    data() {
+      return {
+        count: 0
+      }
+    }
+  })
+  
+  app.mount('#app')*/
